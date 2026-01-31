de134b2785b04f04e24d9c439e5d518c3f1f73d3
shankar1606 <shankarw1606@gmail.com>
2026-01-31 17:20:06 +0530

    Docker_Example-v1

---
+++ src/test/java/com/docker_Exapmle/DockerExapmleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker_Exapmle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExapmleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
